609aa0f53128a9b0fe367e27295c50bfbed0aa6f
adem.demir <adem.demir@akgun.com.tr>
2023-05-29 17:40:50 +0300

    Eddited something

--- LearnFD/src/app/pages/my-account/my-account.page.ts
+++ LearnFD/src/app/pages/my-account/my-account.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { Platform, LoadingController, ActionSheetController, ModalController } from '@ionic/angular';
 import { TranslateService } from '@ngx-translate/core';
 import * as moment from 'moment';
@@ -22,23 +22,6 @@ export class MyAccountPage implements OnInit {
   isDropdownOther = false;
   account = account;
   settings = GeneralSettings;
-  
-  deviceData = {
-    data: [
-      { name: 'PEEP', value: 120, color:"#E29DC6" },
-      { name: 'Temp', value: 52, color:"#F4F4F4" },
-      { name: 'SpO2', value: 86, color:"#FA7C35" },
-      { name: 'PAPd', value: 11, color:"#FC5B5B" },
-      { name: 'HR', value: 29, color:"#F4F4F4" },
-      { name: 'ABPd', value: 3, color:"#F5CA5A" },
-      { name: 'ST-II', value: 98, color:"#F4F4F4" },
-      { name: 'NBPm', value: 142, color:"#5FE3BC" },
-      { name: 'nPR', value: 29, color:"#F5CA5A" },
-      { name: 'MVE', value: 198, color:"#7B8DEC" },
-      { name: 'ST-1', value: 26, color:"#F4F4F4" }
-    ]
-  };
-
     constructor(
     public platform: Platform,
     public c_Utils: C_Utils,
